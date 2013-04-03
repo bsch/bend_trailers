929bc1c02d9c51d21f2c77f586f74d9022ec6acb
Bob Schubert <bobschubert@gmail.com>
2013-04-03 09:28:59 -0700

    Initial commit with db config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BendTrailers::Application.config.secret_token = '2d17feb006c975cb5d2bc6582e3d26a584676c8a23366e4612d06c45f691d263afe010196c5ec3df099348321dac70b1a41156db2fe11ed0ed4a83abe0e42636'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BendTrailers::Application.config.session_store :cookie_store, key: '_bend_trailers_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BendTrailers::Application.config.session_store :active_record_store
