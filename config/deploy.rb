219b488e4baf09d2b2ae70a463893f6b3555e02d
Bob Schubert <bobschubert@gmail.com>
2013-04-06 10:17:24 -0700

    altered afters for unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,7 +34,7 @@ after "bundle:install", "symlink_database_yml"
 namespace :unicorn do
   desc "Zero-downtime restart of Unicorn"
   task :restart, except: { no_release: true } do
-    run "kill -s USR2" # `cat /tmp/unicorn.store.pid`"
+    run "kill -s USR2  `cat /tmp/unicorn.store.pid`"
   end
  
   desc "Start unicorn"
@@ -44,11 +44,11 @@ namespace :unicorn do
  
   desc "Stop unicorn"
   task :stop, except: { no_release: true } do
-    run "kill -s QUIT" #{}`cat /tmp/unicorn.store.pid`"
+    run "kill -s QUIT {}`cat /tmp/unicorn.store.pid`"
   end
 end
  
-after "deploy:restart", "unicorn:restart"
+after "deploy:restart", "unicorn:stop", "unicorn:start"
 
 
 
