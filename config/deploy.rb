a836222ac5555337228122ff97f330983e3221f9
Bob Schubert <bobschubert@gmail.com>
2013-04-07 11:19:44 -0700

    set :scm, :git in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,8 @@ load 'deploy/assets'
 set :application, "store"
 set :repository,  "git@github.com:bsch/bend_trailers.git"
 
+set :scm, :git
+
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
