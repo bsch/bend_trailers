c14fb3dd9667294a155cf4b118494742ab9623ff
Bob Schubert <bobschubert@gmail.com>
2013-04-06 20:50:05 -0700

    cap deploy:migrate failing

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,7 +35,7 @@ after "bundle:install", "symlink_database_yml"
 namespace :unicorn do
   desc "Zero-downtime restart of Unicorn"
   task :restart, except: { no_release: true } do
-    run "kill -s USR2 `cat #{RAILS_ROOT}/#{unicorn_pid}`"
+    run "kill -s USR2 `cat #{unicorn_pid}`"
   end
  
   desc "Start unicorn"
@@ -45,7 +45,7 @@ namespace :unicorn do
  
   desc "Stop unicorn"
   task :stop, except: { no_release: true } do
-    run "kill -s QUIT `cat #{RAILS_ROOT}/#{unicorn_pid}`"
+    run "kill -s QUIT `cat #{unicorn_pid}`"
   end
 end
  
